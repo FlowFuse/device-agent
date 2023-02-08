8aa59fbaaeb9b6070fd685fd6eb2ca1627b9fd5d
Steve-Mcl <sdmclaughlin@gmail.com>
2023-02-08 12:20:49 +0000

    new tests for new AgentManager

---
+++ test/unit/lib/AgentManager_spec.js
@@ -0,0 +1,87 @@
+const should = require('should') // eslint-disable-line
+const sinon = require('sinon')
+const agent = require('../../../lib/agent')
+// const httpClient = require('../../../lib/http')
+// const mqttClient = require('../../../lib/mqtt')
+// const launcher = require('../../../lib/launcher.js')
+const path = require('path')
+const fs = require('fs/promises')
+const os = require('os')
+const { AgentManager } = require('../../../lib/AgentManager')
+
+describe('Test the AgentManager', function () {
+    let configDir
+    beforeEach(async function () {
+        configDir = await fs.mkdtemp(path.join(os.tmpdir(), 'ff-launcher-'))
+        await fs.mkdir(path.join(configDir, 'project'))
+        stubAgent()
+    })
+    afterEach(async function () {
+        await fs.rm(configDir, { recursive: true, force: true })
+        await AgentManager.close()
+        sinon.restore()
+    })
+    it('Create and init the Agent Manager', function () {
+        AgentManager.init({})
+        AgentManager.should.have.property('options').and.eql({})
+        AgentManager.state.should.eql('unknown')
+    })
+    it('Agent Manager should exit cleanly', async function () {
+        AgentManager.init({})
+        await AgentManager.close()
+        AgentManager.exiting.should.be.true()
+    })
+    it('Agent Manager loads config from file (provisioning token)', async function () {
+        const deviceFile = path.join(configDir, 'project', 'device.yml')
+        await fs.writeFile(deviceFile, 'forgeURL: http://localhost:9000\nprovisioningTeam: team1\nprovisioningToken: abcd\n')
+        AgentManager.init({
+            deviceFile
+        })
+        await AgentManager.reloadConfig()
+
+        AgentManager.should.have.property('options')
+        AgentManager.options.should.have.property('deviceFile', deviceFile)
+        AgentManager.should.have.property('configuration')
+        AgentManager.configuration.should.have.property('version').and.be.a.String()
+        AgentManager.configuration.should.have.property('token', 'abcd')
+        AgentManager.configuration.should.have.property('forgeURL', 'http://localhost:9000')
+        AgentManager.configuration.should.have.property('provisioningTeam', 'team1')
+        AgentManager.configuration.should.have.property('provisioningMode', true)
+    })
+    it('Agent Manager loads config from file (regular credentials)', async function () {
+        const deviceFile = path.join(configDir, 'project', 'device.yml')
+        await fs.writeFile(deviceFile, 'deviceId: ididid\nforgeURL: http://localhost:9999\ncredentialSecret: yoohoo\ntoken: bbbb\n')
+        AgentManager.init({
+            deviceFile
+        })
+        await AgentManager.reloadConfig()
+
+        AgentManager.should.have.property('options')
+        AgentManager.options.should.have.property('deviceFile', deviceFile)
+        AgentManager.should.have.property('configuration')
+        AgentManager.configuration.should.have.property('version').and.be.a.String()
+        AgentManager.configuration.should.have.property('deviceId', 'ididid')
+        AgentManager.configuration.should.have.property('token', 'bbbb')
+        AgentManager.configuration.should.have.property('credentialSecret', 'yoohoo')
+        AgentManager.configuration.should.have.property('forgeURL', 'http://localhost:9999')
+        should(AgentManager.configuration.provisioningMode !== true).be.true()
+    })
+    it('Agent Manager should call agent start (regular credentials)', async function () {
+        this.skip() // TODO: fix this test
+        const deviceFile = path.join(configDir, 'project', 'device.yml')
+        await fs.writeFile(deviceFile, 'deviceId: ididid\nforgeURL: http://localhost:9999\ncredentialSecret: yoohoo\ntoken: bbbb\n')
+        AgentManager.init({
+            deviceFile
+        })
+        await AgentManager.startAgent()
+        agent.newAgent.calledOnce.should.be.true()
+        AgentManager.agent.start.calledOnce.should.be.true()
+    })
+})
+
+function stubAgent () {
+    agent.newAgent = sinon.stub().returns({
+        start: sinon.stub().returns('started'),
+        stop: sinon.stub()
+    })
+}
