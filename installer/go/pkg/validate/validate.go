51fdeb4a469211711dc3a73640a18b0ec6a83f8f
PPawlowski <piotr@devopswizards.com>
2025-09-26 16:29:19 +0200

    Properly escape special character in the URL

--- installer/go/pkg/validate/validate.go
+++ installer/go/pkg/validate/validate.go
@@ -293,8 +293,8 @@ func checkFreeDiskSpace(customWorkDir string, requiredBytes uint64) error {
 			freeMB := float64(free) / (1024 * 1024)
 			// err := fmt.Errorf("insufficient disk space in %s (%s): need at least %.1f MB, available %.1f MB", t.label, t.path, requiredMB, freeMB)
 			err := fmt.Errorf("insufficient disk space in %s (%s): need at least %.1f MB, available %.1f MB\n" +
-				"For information on how to handle this error, visit: http://flowfuse.com/docs/device-agent/install/device-agent-installer/#error%3A-disk-space-check-failed",
-    		t.label, t.path, requiredMB, freeMB)
+				"For information on how to handle this error, visit: http://flowfuse.com/docs/device-agent/install/device-agent-installer/#error%%3A-disk-space-check-failed",
+				t.label, t.path, requiredMB, freeMB)
 			logger.LogFunctionExit("checkFreeDiskSpace", nil, err)
 			return err
 		}
