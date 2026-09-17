e180d6c070f8cd9ab8a142d71d3762f5cc6c8629
Ben Hardill <ben@flowforge.com>
2026-09-17 11:55:37 +0100

    Only check combined path

--- frontend/routes.js
+++ frontend/routes.js
@@ -69,13 +69,9 @@ const routes = [
         method: 'GET',
         path: '/assets/*',
         handler: (req, res) => {
-            if (req.url.includes('../') || req.url.includes('..\\')) {
-                res.writeHead(404)
-                res.end()
-                return
-            }
+            const allowedPath = path.join(__dirname, 'assets')
             const assetPath = path.join(__dirname, req.url)
-            if (!fs.existsSync(assetPath)) {
+            if (!assetPath.startsWith(allowedPath) || !fs.existsSync(assetPath)) {
                 res.writeHead(404)
                 res.end()
                 return
